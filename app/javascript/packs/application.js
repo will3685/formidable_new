6409b1836f03f060412cc0b208446929d0dae8c6
Will Theard <lavardwilltheard@yahoo.com>
2022-04-28 00:12:32 -0300

    add footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,11 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+// external imports
+// import "@fortawesome/fontawesome-free/css/all.css"
+import "../stylesheets/application.scss"
+import "bootstrap";
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,5 +21,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+
+//stimulus
 import "controllers"
-import "bootstrap"
+
