b4f96c5fa64742fb674775fc79bffcde332be930
Will <lavardwilltheard@yahoo.com>
2022-05-12 02:09:36 -0300

    add salon model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   add_flash_types :success, :failed
+
+  def after_sign_in_path(resource)
+    feed_path(ressouce.user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get ':user_id/feed', to: 'pages#feed', as: :feed
 
+  resources :salons do
+    collection do 
+      get :allthings
+    end
+  end
 end
