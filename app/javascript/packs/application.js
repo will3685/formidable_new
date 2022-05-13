e87b1f7dbcc37419a3de26f5ef3265e98aca3e79
Will <lavardwilltheard@yahoo.com>
2022-05-13 01:33:24 -0300

    config react in rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,3 +30,7 @@ document.addEventListener('turbolinks:load', () => {
   displayAlert();
 })
 
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
