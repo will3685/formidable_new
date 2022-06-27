a746f70c42bf31158e03f00bce8e11e60e34a1d6
Will <lavardwilltheard@yahoo.com>
2022-06-27 19:51:46 -0300

    set favorite function uniqueness

--- app/controllers/favoritos_controller.rb
+++ app/controllers/favoritos_controller.rb
@@ -9,6 +9,12 @@ class FavoritosController < ApplicationController
     @favoritos = Favorito.where(user_id: current_user.id)
   end
 
+  def destroy
+    @favorito = Favorito.find_by(salon_id: params[:salon_id])
+    @favorito.destroy
+    redirect_to user_favoritos_path(current_user.id), notice: "Favorite salon deleted"
+  end
+
   private 
 
   def favoritos_params
--- app/models/favorito.rb
+++ app/models/favorito.rb
@@ -1,4 +1,5 @@
 class Favorito < ApplicationRecord
   belongs_to :salon
   belongs_to :user
+  validates :salon_id, uniqueness: { scope: :user_id}
 end
